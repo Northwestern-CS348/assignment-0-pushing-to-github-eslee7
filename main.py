6020217c3b21c19c39ff1918fb2f6aa03bd06ed3
Elise Lee <eliselee2021@u.northwestern.edu>
2019-01-14 14:48:15 -0600

    assignment 0

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ class SetupTest(unittest.TestCase):
 		result = student_code.shouldReturnTrue()
 		self.assertTrue(result)
 
+	def test02(self):
+		result = student_code.shouldReturnTrue()
+		self.assertEqual(True, result)
+
 if __name__ == '__main__':
 	unittest.main()
